485d64661c2ec4296a7a7eb2e1ca8db85f1d6c03
Henry Kam <h3nrykam@gmail.com>
2021-10-27 11:12:08 -0700

    add trip

---
+++ models/Trip.js
@@ -0,0 +1,47 @@
+const {Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Trip extends Model {}
+
+Trip.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        trip_budget: {
+            type: DataTypes.DECIMAL,
+            allowNull: false,
+            validate: {
+                isDecimal: true
+            }
+        },
+        traveller_amount: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        traveller_id:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: "traveler",
+                key: "id"
+            }
+        },
+        location_id:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: "location",
+                key: "id"
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        modelName: 'trip'
+    }
+)
